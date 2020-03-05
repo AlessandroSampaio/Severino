70a58be6ba84be22c6f2c7811c0c3e4023bd5630
AlessandroSampaio <alessandrosampaio521@gmail.com>
2020-03-05 08:32:55 -0300

    Adicionada models de Usuario e Requisicao

---
+++ Models/Requisicao.cs
@@ -0,0 +1,19 @@
+﻿using Severino.Models.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace Severino.Models
+{
+    public class Requisicao
+    {
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+        public string Detalhes { get; set; }
+        public double ValorRemuneracao { get; set; }
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+        public Usuario Autor { get; set; }
+        public List<Usuario> Candidatos { get; set; }
+        public RequisicaoStatus Status { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Severino.Models
+{
+	public class Usuario : IdentityUser
+	{
+		public bool PrestadorDeServico { get; set; }
+		public double AvaliacaoPrestador { get; set; }
+		public double AvaliacaoContratante { get; set; }
+	}
+}
